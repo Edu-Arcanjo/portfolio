0c9c2a7f93448108e4412cdcdc8af16eb639b6c3
Eduardo Santos <57601135+Edu-Arcanjo@users.noreply.github.com>
2023-08-05 12:58:29 -0300

    feat(hero): append features grid on hero bottom
    Add 3 cards with a list of features to the bottom of hero. Between the hero and the next section

---
+++ src/app/components/Features.tsx
@@ -0,0 +1,94 @@
+import {
+  BirdIcon,
+  BriefcaseIcon,
+  CalendarIcon,
+  CheckCircleIcon,
+  MedalIcon,
+  StarIcon,
+  Users2Icon,
+} from "lucide-react";
+import React from "react";
+
+function getAge(dateString: string) {
+  let today = new Date();
+  let birthDate = new Date(dateString);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  let m = today.getMonth() - birthDate.getMonth();
+
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate() + 1)) age--;
+
+  return age;
+}
+
+export default function Features() {
+  const age = getAge("2001-02-27");
+  const jobAge = getAge("2021-05-01");
+
+  return (
+    <div className="px-2">
+      <div className="container mx-auto flex gap-12">
+        <div className="-mt-20 grid w-full grid-cols-1 justify-center gap-y-8 md:grid-cols-2 lg:grid-cols-3">
+          <div className="mx-auto w-full max-w-xs border border-slate-200 bg-slate-50 p-8 shadow-md">
+            <h2 className="mb-4 text-3xl font-bold text-slate-900">
+              Quem sou?
+            </h2>
+            <ul className="flex flex-col gap-3 text-lg">
+              <li className="flex items-center">
+                <BriefcaseIcon className="mr-2 text-sky-500" /> Desenvolvedor
+                frontend
+              </li>
+              <li className="flex items-center">
+                <BirdIcon className="mr-2 text-sky-500" /> Há {jobAge} anos como
+                frelancer
+              </li>
+              <li className="flex items-center">
+                <CalendarIcon className="mr-2 text-sky-500" /> Tenho {age} anos
+              </li>
+            </ul>
+          </div>
+          <div className="mx-auto w-full max-w-xs border border-slate-200 bg-slate-50 p-8 shadow-md">
+            <h2 className="mb-4 text-3xl font-bold text-slate-900">
+              E no Workana?
+            </h2>
+            <ul className="flex flex-col gap-3 text-lg">
+              <li className="flex items-center">
+                <MedalIcon className="mr-2 text-sky-500" />
+                Tier <strong className="ml-1 text-amber-600">Gold</strong>
+              </li>
+              <li className="flex items-center">
+                <StarIcon className="mr-2 text-sky-500" />
+                Classifição média 4.3/5.0
+              </li>
+              <li className="flex items-center">
+                <Users2Icon className="mr-2 text-sky-500" />7 clientes
+                satisfeitos
+              </li>
+            </ul>
+          </div>
+          <div className="mx-auto w-full max-w-xs border border-slate-200 bg-slate-50 p-8 shadow-md md:hidden lg:block">
+            <h2 className="mb-4 text-3xl font-bold text-slate-900">
+              E no GitHub?
+            </h2>
+            <ul className="flex flex-col gap-3 text-lg">
+              <li className="flex items-center">
+                <b className="mr-2 text-sky-600">82%</b> Commits
+              </li>
+              <li className="flex items-center">
+                <b className="mr-2 text-sky-600">10%</b> Pull requests
+              </li>
+              <li className="flex items-center">
+                <b className="mr-2 text-sky-600">07%</b> Issues
+              </li>
+              <li className="flex items-center">
+                <b className="mr-2 text-sky-600">01%</b> Code review
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="mt-4 text-center text-sm text-slate-600">
+        *Dados do Workana e GitHub de Julho de 2022
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,110 +1,13 @@
-import background from "@/assets/background.jpg";
-import tile from "@/assets/tile.svg";
-import { FileBadge, GithubIcon, LinkedinIcon } from "lucide-react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/Tooltip";
-
-const links = [
-  {
-    icon: LinkedinIcon,
-    href: "https://www.linkedin.com/in/eduardo-santos-86b07020b/",
-    hover: "Linkedin",
-    target: "blank",
-  },
-  {
-    icon: GithubIcon,
-    href: "https://github.com/Edu-Arcanjo",
-    hover: "GitHub",
-    target: "blank",
-  },
-  {
-    icon: FileBadge,
-    href: "#",
-    hover: "Currículo",
-  },
-];
-
-function getAge(dateString: string) {
-  let today = new Date();
-  let birthDate = new Date(dateString);
-  let age = today.getFullYear() - birthDate.getFullYear();
-  let m = today.getMonth() - birthDate.getMonth();
-
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate() + 1)) age--;
-
-  return age;
-}
+import Header from "./components/Header";
+import Features from "./components/Features";
 
 export default function Home() {
-  const age = getAge("2001-02-27");
-  const jobAge = getAge("2021-05-01");
-
   return (
     <>
-      <header id="hero" className="relative h-screen">
-        {/* background */}
-        <div
-          className="absolute inset-0 bg-slate-50 bg-center bg-no-repeat"
-          style={{ backgroundImage: `url(${background.src})` }}
-        >
-          <div
-            className="absolute inset-0 bg-center"
-            style={{
-              backgroundImage: `url(${tile.src})`,
-              WebkitMaskImage: "linear-gradient(transparent, black)",
-            }}
-          />
-        </div>
-
-        {/* content */}
-        <div className="relative mx-auto flex max-w-5xl flex-col justify-center pt-20 sm:pt-24 lg:pt-32">
-          <div className="relative mx-auto">
-            <p className="ml-2 text-xl font-bold text-slate-700">
-              Oi, 👋🏾 sou o Eduardo
-            </p>
-            <h1 className="relative text-4xl font-black uppercase tracking-wide text-slate-900 sm:text-5xl lg:text-9xl">
-              <span>
-                React
-                <br />
-                Developer
-              </span>
-            </h1>
-            <div className="flex justify-end">
-              <p className="max-w-md text-end text-xl font-bold text-slate-700">
-                Desenvolvedor frontend há {jobAge} anos como PJ
-                <br />
-                {age} anos
-              </p>
-            </div>
-            <div className="absolute right-3 top-16">
-              <ul className="flex space-x-8">
-                {links.map(({ icon: Icon, href, target, hover }) => (
-                  <li>
-                    <TooltipProvider delayDuration={0}>
-                      <Tooltip>
-                        <TooltipTrigger>
-                          <a
-                            href={href}
-                            className="flex h-16 w-16 items-center justify-center rounded-lg bg-sky-100 text-sky-900 shadow-md ring-1 ring-inset ring-sky-400 transition-all duration-300 hover:bg-sky-200 hover:text-sky-950 hover:ring"
-                            target={target}
-                          >
-                            <Icon className="h-10 w-10" />
-                          </a>
-                        </TooltipTrigger>
-                        <TooltipContent>{hover}</TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
+      <main className="pb-24 tracking-wide">
+        <Features />
+      </main>
     </>
   );
 }
